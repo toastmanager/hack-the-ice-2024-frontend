069358a210c7b64d52fc84ced7b0b3cbd9670e90
toastmanager <mrbonkyou@gmail.com>
2024-11-05 11:55:50 +0900

    style: make closer to design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,69 +2,95 @@ import { Icon } from "@iconify/react";
 
 const Footer = () => {
   return (
-    <div className="w-screen w-96 bg-green-400 relative">
-            <div className="px-36 pt-4 pb-5 flex grid grid-cols-4 gap-4 ">
-                    <div className="space-y-2">
-                        <h1 className="pb-5">Главное</h1>
-                        <h1>Все авторские туры</h1>
-                        <h1>Владельцам туров</h1>
-                        <h1>Поддержка</h1>
-                        <h1>Правила оплаты</h1>
-                        <h1>Реквизиты организации</h1>
-                        
-                    </div>
-                    <div className="space-y-2">
-                        <h1 className="pb-5">Помощь путешественнику</h1>
-                        <div className="flex items-center">
-                            <Icon icon="ic:baseline-mail" /> <h1>info@yatvoiorientir.com</h1>
-                        </div>
-                        <div className="flex items-center">
-                            <Icon icon="mdi:phone" /> <h1>+7(999)173-80-47</h1>
-                        </div>
-                        <div className="flex items-center">
-                            <Icon icon="fa-solid:building" /> <h1>г. Якутск улица Ленина 1, каб 1488</h1>
-                        </div>
-                    </div>
+    <section className="w-full bg-card px-3 py-5">
+      <div className="m-auto max-w-[1200px] flex flex-wrap gap-3 text-sm justify-between items-center leading-7">
+        <div className="flex flex-col">
+          <span className="pb-2 text-lg font-semibold text-foreground-alternative">
+            Главное
+          </span>
+          <a href="#">Все авторские туры</a>
+          <a href="#">Владельцам туров</a>
+          <a href="#">Поддержка</a>
+          <a href="#">Правила оплаты</a>
+          <a href="#">Реквизиты организации</a>
+        </div>
 
-                    <div className="space-y-2">
-                        <h1 className="pb-5">Помощь организатору</h1>
-                        <div className="flex items-center">
-                            <Icon icon="ic:baseline-mail" /> <h1>info@yatvoiorientir.com</h1>
-                        </div>
-                        <div className="flex items-center">
-                            <Icon icon="mdi:phone" /> <h1>+7(999)173-80-47</h1>
-                        </div>
-                        <div className="flex items-center">
-                            <Icon icon="fa-solid:building" /> <h1>г. Якутск улица Ленина 1, каб 1488</h1>
-                        </div>
-                    </div>
-                    <div className="space-y-2">
-                    <h1 className="pb-5">Платежные системы</h1>
-                    <div className="grid grid-cols-3 gap-4">
-                        <div className="bg-card flex items-center justify-center px-4 py-3 rounded-3xl">
-                        <Icon icon="logos:visa" width="40" />
-                        </div>
-                        <div className="bg-card flex items-center justify-center px-4 py-3 rounded-3xl">
-                        <Icon icon="logos:mastercard" width="40" />
-                        </div>
-                        <div className="bg-card flex items-center justify-center px-4 py-3 rounded-3xl">
-                        <h1>mir epta</h1>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                <div className="absolute bottom-2">
-                    <h1>logo here</h1>
-                </div>
+        <div>
+          <span className="text-base">Помощь путешественнику</span>
+          <div className="flex items-center gap-1 mt-2">
+            <Icon
+              icon="ic:baseline-mail"
+              className="text-foreground-alternative"
+            />
+            <span>info@yatvoiorientir.com</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Icon
+              icon="mage:phone-fill"
+              className="text-foreground-alternative"
+            />
+            <span>+7(999)173-80-47</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Icon
+              icon="fa-solid:building"
+              className="text-foreground-alternative"
+            />
+            <span>г. Якутск улица Ленина 1, каб 1488</span>
+          </div>
+        </div>
+
+        <div>
+          <span className="text-base">Помощь организатору</span>
+          <div className="flex items-center gap-1 mt-2">
+            <Icon
+              icon="ic:baseline-mail"
+              className="text-foreground-alternative"
+            />
+            <span>info@yatvoiorientir.com</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Icon
+              icon="mage:phone-fill"
+              className="text-foreground-alternative"
+            />
+            <span>+7(999)173-80-47</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Icon
+              icon="fa-solid:building"
+              className="text-foreground-alternative"
+            />
+            <span>г. Якутск улица Ленина 1, каб 1488</span>
+          </div>
+        </div>
+
+        <div className="">
+          <span className="text-base">Платежные системы</span>
+          <div className="grid grid-cols-3 gap-4 mt-2">
+            <div className="bg-background flex items-center justify-center px-4 py-3 rounded-lg">
+              <Icon
+                icon="logos:visa"
+                width="40"
+                className="text-foreground-alternative"
+              />
             </div>
-
-            <div className="w-full text-center mt-12">
-          <p className="text-sm text-gray-500">© 2024 Your Company Name. All rights reserved.</p>
-          
-          
+            <div className="bg-background flex items-center justify-center px-4 py-3 rounded-lg">
+              <Icon icon="logos:mastercard" width="40" />
+            </div>
+            <div className="bg-background flex items-center justify-center px-4 py-3 rounded-lg">
+              <span>МИР</span>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div className="w-full text-center mt-12">
+        <span className="text-base font-semibold text-gray-500">
+          Все права защищены © 2024 Якутия: Твой ориентир
+        </span>
+      </div>
+    </section>
   );
 };
 
