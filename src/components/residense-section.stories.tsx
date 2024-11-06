d3b8d7771f0984aa1816268bde8deb39c90d1899
toastmanager <mrbonkyou@gmail.com>
2024-11-06 22:50:34 +0900

    refactor:

--- src/components/residense-section.stories.tsx
+++ src/components/residense-section.stories.tsx
@@ -17,15 +17,17 @@ export const Default: Story = {
       comfort: 4,
       motelDuration: "7 дней 6 ночи",
       hotelDuration: "4 ночи",
-      items: [{
-        duration: "6 ночей",
-        title: "Irkutsk city center",
-        imagesUrls: [
-          "https://broni.travel/public/sites/pages/5784/380139.jpeg",
-        ],
-        description:
-          "Мы будем жить в уютном гест хаусе с красивой зелёной территорией c видом на пальмовую рощу в шаговой доступности от океана. В номерах есть всё необходимое для вашего удобства: кондиционер, фен, электрический чайник, сейф, wi-fi.",
-      }],
+      items: [
+        {
+          duration: "6 ночей",
+          title: "Irkutsk city center",
+          imagesUrls: [
+            "https://broni.travel/public/sites/pages/5784/380139.jpeg",
+          ],
+          description:
+            "Мы будем жить в уютном гест хаусе с красивой зелёной территорией c видом на пальмовую рощу в шаговой доступности от океана. В номерах есть всё необходимое для вашего удобства: кондиционер, фен, электрический чайник, сейф, wi-fi.",
+        },
+      ],
     },
   },
 };
