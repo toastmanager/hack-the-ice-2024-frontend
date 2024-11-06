19197df29a349d612e823943b3fdf8d667945aa5
toastmanager <mrbonkyou@gmail.com>
2024-11-06 11:54:59 +0900

    feat(auth): useAuth and from it;

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,16 +1,23 @@
+"use client";
+
 import Link from "next/link";
 import { Icon } from "@iconify/react";
 import { Input } from "./ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
+import { useAuth } from "@/providers/auth-provider";
 
 const AppBar = () => {
+  const user = useAuth();
+
   return (
     <nav className="flex justify-center bg-card text-card-foreground py-3 px-3">
       <div className="w-[1200px] flex justify-between text-sm">
         <div className="flex gap-7 items-center">
-          <Image alt="Logo" src={logo} />
+          <Link href='/'>
+            <Image alt="Logo" src={logo} />
+          </Link>
           <Input placeholder="Поиск" className="w-60 bg-accent" />
           <div className="flex gap-5">
             <Link href={"#"}>Все путешествия</Link>
@@ -26,10 +33,20 @@ const AppBar = () => {
               className="text-accent-foreground"
             />
           </button>
-          <Avatar>
-            <AvatarImage />
-            <AvatarFallback>User</AvatarFallback>
-          </Avatar>
+          <Link href={!user ? "/auth" : `/users/${user.id}`}>
+            {!user ? (
+              <div className="p-2 bg-accent rounded-full">
+                <Icon icon={"mage:login"} width={24} />
+              </div>
+            ) : (
+              <Avatar>
+                <AvatarImage src={user.avatarUrl} />
+                <AvatarFallback className="bg-primary text-primary-foreground text-3xl font-medium">
+                  {user.fullname[0]}
+                </AvatarFallback>
+              </Avatar>
+            )}
+          </Link>
         </div>
       </div>
     </nav>
