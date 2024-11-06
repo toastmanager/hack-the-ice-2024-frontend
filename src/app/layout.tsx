2a24372160cfe4b24f3a9a95456d4b215174c14f
toastmanager <mrbonkyou@gmail.com>
2024-11-06 09:24:31 +0900

    feat(auth): auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/providers/auth-provider";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={`h-full ${inter.className} antialiased`}>
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
---
+++ src/providers/auth-provider.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import api from "@/lib/api-client";
+import { createContext, useEffect, useLayoutEffect, useState } from "react";
+
+const AuthContext = createContext<UserEntity | null>(null);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<UserEntity | null>(null);
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [isRetry, setIsRetry] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        const response = await api.get("auth/me");
+        setUser(response.data);
+      } catch (error) {
+        setUser(null);
+      }
+    };
+
+    const fetchAccessToken = async () => {
+      try {
+        const response = await api.get("auth/refresh");
+        setAccessToken(response.data.access_token);
+      } catch (error) {
+        setUser(null);
+      }
+    };
+
+    fetchAccessToken();
+    fetchMe();
+  }, []);
+
+  useLayoutEffect(() => {
+    const authInterceptor = api.interceptors.request.use((config) => {
+      config.headers.Authorization =
+        !isRetry && accessToken
+          ? `Bearer ${accessToken}`
+          : config.headers.Authorization;
+      return config;
+    });
+
+    return () => {
+      api.interceptors.request.eject(authInterceptor);
+    };
+  }, [accessToken]);
+
+  useLayoutEffect(() => {
+    const refreshInterceptor = api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401) {
+          try {
+            const response = await api.get("api/refresh");
+
+            setAccessToken(response.data.access_token);
+
+            originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`;
+
+            return api(originalRequest);
+          } catch (error) {
+            setAccessToken(null);
+            setUser(null);
+            setIsRetry(false);
+          }
+        }
+      }
+    );
+
+    return () => {
+      api.interceptors.response.eject(refreshInterceptor);
+    };
+  });
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
