6c3661b9d14ffb9054181333d30ea1599f954163
toastmanager <mrbonkyou@gmail.com>
2024-11-06 23:25:54 +0900

    style: make close to design

--- src/components/author-contact.tsx
+++ src/components/author-contact.tsx
@@ -2,33 +2,51 @@ import { Icon } from "@iconify/react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "@/components/ui/button";
 
-const AuthorContact = ({ authorcontact }: { authorcontact: AuthorContactEntity }) => {
+const AuthorContact = ({
+  authorcontact: authorContact,
+}: {
+  authorcontact: AuthorContactEntity;
+}) => {
   return (
-    <div className="items-start relative my-4 border rounded-md p-3 px-6 bg-white w-96 space-y-4">
-        <div className="flex">
-      <Avatar>
+    <div className="flex rounded-md bg-card p-5 justify-between">
+      <div className="w-[540px] space-y-5">
+        <div>
+          <h1 className="text-3xl font-semibold">
+            Автор туров{" "}
+            {authorContact.authorName.split(" ").slice(0, 2).join(" ")}
+          </h1>
+          {authorContact.isVerified && (
+            <div className="flex items-center gap-x-2">
+              <Icon
+                icon={"mage:check-circle-fill"}
+                className="text-primary w-5 h-5"
+              />
+              <span className="font-medium text-base">
+                Проверенный организатор
+              </span>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <span className="">{authorContact.description}</span>
+        </div>
+
+        <Button
+          variant="outline"
+          className="w-full bg-card hover:bg-card border-primary text-primary"
+        >
+          Написать автору
+        </Button>
+      </div>
+
+      <Avatar className="w-28 h-28">
         <AvatarImage
-          src={authorcontact.avatarUrl}
+          src={authorContact.avatarUrl}
           alt="Avatar"
-          className="w-16 h-16 rounded-full"
+          className="object-cover object-left-top"
         />
       </Avatar>
-
-      <div className="w-full pl-2 pt-1 flex flex-col justify-between">
-        <div className="space-y-1">
-          <h1 className="text-xs text-gray-400">Автор Тура</h1>
-          <h1 className="text-xs">{authorcontact.authorName}</h1>
-          <p className="text-sm text-gray-600"></p>
-        </div>
-        </div>
-        
-      </div>
-      <Button
-            variant="outline"
-            className="w-full bg-card hover:bg-card border-primary text-primary"
-          >
-            Написать автору
-          </Button>
     </div>
   );
 };
--- src/entities/author-contact-entity.ts
+++ src/entities/author-contact-entity.ts
@@ -1,5 +1,7 @@
 interface AuthorContactEntity {
     authorName: string;
     avatarUrl: string;
+    isVerified: boolean;
+    description: string;
   }
   
