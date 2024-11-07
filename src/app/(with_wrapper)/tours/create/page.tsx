098015f9f9e7d1ab2f0065d2c4ef5a0c6cb3a545
toastmanager <mrbonkyou@gmail.com>
2024-11-08 05:44:34 +0900

    feat: step change

--- src/app/(with_wrapper)/tours/create/page.tsx
+++ src/app/(with_wrapper)/tours/create/page.tsx
@@ -1,13 +1,31 @@
+"use client";
+
 import StepsBar from "@/components/stepsbar";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const TourCreatePage = () => {
+  const [step, setStep] = useState(1);
+
   return (
-    <div className="h-full w-full flex justify-center">
+    <div className="h-full w-full flex justify-center px-3">
       <main className="max-w-[1200px] w-full mt-5 space-y-5">
         <span className="text-2xl font-semibold">Создание тура</span>
-        <StepsBar step={1} />
-        <Button />
+        <StepsBar step={step} />
+        <div className="flex gap-2">
+          {step > 1 && (
+            <Button
+              className="w-full bg-card text-primary hover:bg-card"
+              variant="outline"
+              onClick={() => setStep(step-1)}
+            >
+              Предыдущий шаг
+            </Button>
+          )}
+          <Button className="w-full" onClick={() => setStep(step + 1)}>
+            Следующий шаг
+          </Button>
+        </div>
       </main>
     </div>
   );
