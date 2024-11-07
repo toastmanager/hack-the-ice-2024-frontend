2eb215735cfc48bc55f2c7a9e3e7cafcd428e44e
toastmanager <mrbonkyou@gmail.com>
2024-11-08 06:42:04 +0900

    feat(tour-create): add tour buttons

--- src/app/(with_wrapper)/tours/create/tour-details-form-screen.tsx
+++ src/app/(with_wrapper)/tours/create/tour-details-form-screen.tsx
@@ -1,9 +1,26 @@
+import TourButton from "@/components/tour-button";
 import TourDescriptionInput from "@/components/tour-description-input";
 
 const TourDetailsFormScreen = () => {
   return (
     <>
       <TourDescriptionInput />
+      <TourButton
+        tourbutton={{
+          title: "Впечатления",
+          description:
+            "Опишите основные впечатления (или места), которые ждут путешественников",
+          btnText: "Добавить",
+        }}
+      />
+      <TourButton
+        tourbutton={{
+          title: "Важная информация о туре",
+          description:
+            "Все вопросы попадут в блок “Важно”. Отметьте нужные вопросы и заполните поле с текстом или создайте свой вариант вопроса.",
+          btnText: "Добавить вопрос",
+        }}
+      />
     </>
   );
 };
