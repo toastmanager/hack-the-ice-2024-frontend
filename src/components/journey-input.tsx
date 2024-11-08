15505dda5959f4bb65b2eca0d63a6da507c645b2
toastmanager <mrbonkyou@gmail.com>
2024-11-08 14:06:37 +0900

    feat: add form support to tourStartEndInput

--- src/components/journey-input.tsx
+++ src/components/journey-input.tsx
@@ -1,42 +1,98 @@
 import { Input } from "@/components/ui/input";
 import { UseFormReturn } from "react-hook-form";
-import { FormField } from "./ui/form";
+import { FormControl, FormField, FormItem } from "./ui/form";
 
 const TourStartEndInput = ({ form }: { form: UseFormReturn }) => {
   return (
     <div className="bg-card text-card-foreground w-full rounded-3xl p-5">
       <div>
-        <span className="text-2xl font-semibold">Начало тура</span>
+        <span className="text-2xl font-semibold">Начало путешесетвия</span>
         <div className="space-y-3 pt-4">
           <FormField
             control={form.control}
             name="start_city"
             render={({ field }) => (
               <Input
-                placeholder="Название тура"
+                placeholder="Старт путешествия (Город)"
                 className="bg-card"
                 {...field}
               />
             )}
           />
           <div className="flex gap-2">
-            <Input placeholder="Тип тура" className="w-2/3 bg-card" />
-            <Input
-              placeholder="Дополнительные типы тура"
-              className="w-1/3 bg-card"
+            <FormField
+              control={form.control}
+              name="start_location"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      placeholder="Место сбора"
+                      className="w-full bg-card"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="start_time"
+              render={({ field }) => (
+                <Input
+                  placeholder="Время сбора"
+                  className="w-1/3 bg-card"
+                  {...field}
+                />
+              )}
             />
           </div>
         </div>
 
         <div className="pt-3">
-          <span className="text-2xl font-semibold ">Конец тура</span>
+          <span className="text-2xl font-semibold ">Финиш путешесетвия</span>
           <div className="space-y-3 pt-4">
-            <Input placeholder="Название тура" className="bg-card" />
+            <FormField
+              control={form.control}
+              name="end_city"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      placeholder="Конец путешествия (Город)"
+                      className="bg-card w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
             <div className="flex gap-2">
-              <Input placeholder="Тип тура" className="w-2/3 bg-card" />
-              <Input
-                placeholder="Дополнительные типы тура"
-                className="w-1/3 bg-card"
+              <FormField
+                control={form.control}
+                name="end_location"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormControl>
+                      <Input
+                        placeholder="Место сбора"
+                        className="w-full bg-card"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="end_time"
+                render={({ field }) => (
+                  <Input
+                    placeholder="Время завершения тура"
+                    className="w-1/3 bg-card"
+                    {...field}
+                  />
+                )}
               />
             </div>
           </div>
